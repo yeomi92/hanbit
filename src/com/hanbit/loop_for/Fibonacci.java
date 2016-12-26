36206b06e8243f2c03cce615dabdbdacea9266e4
yheisun <yheisun@gmail.com>
2016-12-26 18:29:16 +0900

    for statement

--- src/com/hanbit/loop_for/Fibonacci.java
+++ src/com/hanbit/loop_for/Fibonacci.java
@@ -1,21 +1,21 @@
 package com.hanbit.loop_for;
+import java.util.Scanner;
 /**
 @FILE  : Fibonacci.java
 @DATE  : 2016. 12. 26.
 @AUTHOR: Yeom Hye-Seon
 @STORY :
  */
-public class Fibonacci {
+public class Fibonacci{
 	public static void main(String[] args) {
 		//1, 1, 2, 3, 5, 8, 13, 21, 34, 55ÀÇ ÇÕ
 		int a=1, b=1, c=2, sum=2;
-		for(int i=3;i<6;i++){
+		for(int i=3;i<=10;i++){
 			c=a+b;
-			a=b+c;
-			b=c+a;
-			System.out.println(c+" "+a+" "+b);
-			sum+=a+b+c;
+			a=b;
+			b=c;
+			sum+=c;
 		}
-		System.out.println(sum-b);
+		System.out.println(sum);
 	}
 }
