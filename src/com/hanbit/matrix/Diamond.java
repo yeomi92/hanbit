5e88c492137091fe1a22f7f2c60bc27d893de142
yheisun <yheisun@gmail.com>
2016-12-30 09:03:24 +0900

    16.12.30

--- src/com/hanbit/matrix/Diamond.java
+++ src/com/hanbit/matrix/Diamond.java
@@ -13,18 +13,20 @@ package com.hanbit.matrix;
 public class Diamond {
 	public static void main(String[] args) {
         int len=5, col= len, row= len, val=0, start=0, end=0; //배열에 저장할 변수
-        int[][] arr = new int[ col][ row];
+        int[][] arr = new int[col][row];
         start=3; //열의 시작 위치
         end=3; //열의 끝 위치
-        for(col=0;col<arr.length;col++){
-        	for(row=0;row<arr[col].length;row++){
-        		arr[col][row]=0;
-        		if(){
-        			arr[col][row]=(val+=1);
-        		}
-        		System.out.print(arr[col][row]);
-        	}
-        	System.out.println("");
+        for(int i=0;i<arr.length;i++){
+         for(int j=0;j<arr[i].length;j++){
+          if(i==2||j==2||i+j==4&&Math.abs(i-j)==2||i==j&&i==1||i==j&&i==3){
+           val++;
+           arr[i][j]=val; 
+          }else{
+           arr[i][j]=0; 
+          }
+          System.out.printf("%2d\t",arr[i][j]);
+         }
+         System.out.println("");
         }
     }
 }
