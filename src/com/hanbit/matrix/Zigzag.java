474a02b38bc5ef61562c48411d65bd347ff58952
yheisun <yheisun@gmail.com>
2016-12-30 10:37:19 +0900

    Zigzag update

--- src/com/hanbit/matrix/Zigzag.java
+++ src/com/hanbit/matrix/Zigzag.java
@@ -1,4 +1,7 @@
 package com.hanbit.matrix;
+
+import java.util.Scanner;
+
 /**
 @FILE  : Zigzag.java
 @DATE  : 2016. 12. 29.
@@ -12,7 +15,11 @@ package com.hanbit.matrix;
 **/
 public class Zigzag {
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
         int length = 5; // 배열 길이
+        System.out.print("Num: ");
+        length=s.nextInt();
+        System.out.printf("%d X %d\n",length,length);
         int[][] arr = new int[length][length];
         int k = 1; // 넣을 값
         int flag = 1; // 스위치변수
@@ -23,11 +30,11 @@ public class Zigzag {
         		if(i==0&&j==0){
         			arr[i][j]=1;
         		}else{
-        		arr[i][j]=k+=1*((i==1||i==3)?-flag:flag);
+        			arr[i][j]=k+=1*((i%2==0)?flag:-flag);
         		}
         		System.out.printf("%2d ",arr[i][j]);
         	}
-        	k+=(i==0||i==2||i==4)?6:4;
+        	k+=(i%2==0)?length+1:length-1;
         	System.out.println("");
         }
     }
