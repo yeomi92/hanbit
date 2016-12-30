4f64ac4afa57c6ff3488e13646901e293c4d7bbe
yheisun <yheisun@gmail.com>
2016-12-30 10:23:43 +0900

    daimond update

--- src/com/hanbit/matrix/Diamond.java
+++ src/com/hanbit/matrix/Diamond.java
@@ -1,9 +1,10 @@
 package com.hanbit.matrix;
+import java.util.Scanner;
 /**
 @FILE  : Diamond.java
 @DATE  : 2016. 12. 29.
 @AUTHOR: Yeom Hye-Seon
-@STORY :
+@STORY : 숫자 입력 받아서 다이아몬드로 숫자 출력
 0    0    1    0    0   
 0    2    3    4    0   
 5    6    7    8    9   
@@ -12,17 +13,21 @@ package com.hanbit.matrix;
 **/
 public class Diamond {
 	public static void main(String[] args) {
-        int len=5, col= len, row= len, val=0, start=0, end=0; //배열에 저장할 변수
+        int len=0, val=0, start=0, end=0; //배열에 저장할 변수int[][] arr = new int[col][row];
+        Scanner s = new Scanner(System.in);
+        System.out.println("Num: ");
+        len=s.nextInt();
+        int col= len, row= len;
         int[][] arr = new int[col][row];
-        start=3; //열의 시작 위치
-        end=3; //열의 끝 위치
+        start=len/2; //열의 시작 위치
+        end=len/2; //열의 끝 위치
         for(int i=0;i<arr.length;i++){
          for(int j=0;j<arr[i].length;j++){
-        	 arr[i][j]=(j>=start-1&&j<=end-1)?++val:0;
-        	 if(i<2&&j==arr[i].length-1){
+        	 arr[i][j]=(j>=start&&j<=end)?++val:0;
+        	 if(i<(len/2)&&j==arr[i].length-1){
     			 start--;
     			 end++;
-    		 }else if(i>1&&j==arr[i].length-1){
+    		 }else if(i>=(len/2)&&j==arr[i].length-1){
     			 start++;
     			 end--;
     		 }
